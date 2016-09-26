91e0f08b1ed4ec6edb28fd7aaa8e272d7266c68e
Samuel Persian <samuel.persian@gmail.com>
2016-09-26 15:56:52 -0600

    map working on home page

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,25 +1,21 @@
 
-$(function () {
-   $("#showMyLocation").click(function (event) {
-       event.preventDefault();
-       $(this).html('Determining address...');
-       navigator.geolocation.getCurrentPosition(function (position) {
-           var geocoder = new google.maps.Geocoder();
-           var latLng   = new google.maps.LatLng(
-               position.coords.latitude, position.coords.longitude);
-           geocoder.geocode({
-               'latLng': latLng
-           }, function (results, status) {
-               for (var i = 0; i < results[0].address_components.length; i++) {
-                   var address = results[0].address_components[i];
-                   if (address.types[0] == "postal_code") {
-                       $('#zipcode').html(address.long_name);
-                       $('#location').html(results[0].formatted_address);
-                       $('#showMyLocation').hide();
-                   }
-               }
-           });
-       });
-       return false;
-   });
-});
+function initMap() {
+  let lat, lon;
+  navigator.geolocation.getCurrentPosition(function (position) {
+      var geocoder = new google.maps.Geocoder();
+      var latLng   = new google.maps.LatLng(
+          position.coords.latitude, position.coords.longitude);
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      console.log("Lat: "+typeof position.coords.latitude);
+      console.log("Lon: "+typeof position.coords.longitude);
+      var myLatLng = {lat: position.coords.latitude, lng: position.coords.longitude};
+
+      // Create a map object and specify the DOM element for display.
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        scrollwheel: false,
+        zoom: 15
+      });
+  });
+}
