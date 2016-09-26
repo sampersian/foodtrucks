fa25b9c155d1c190d9ac0236afb9b8f73eb6e607
Dunbarry <RyanADunbar@gmail.com>
2016-09-26 15:25:15 -0600

    Geocoding function in main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,3 +12,15 @@ function showForm(aForm) {
   let formName = aForm+"SignupForm";
   $('.'+formName).show()
 }
+
+function geoCoder(address){
+     var formattedAddress = address.split(' ').join('+');
+  $.ajax({
+    url:'https://maps.googleapis.com/maps/api/geocode/json?address='+formattedAddress+'&key=AIzaSyAMCg6786tQQUE9PoC4RNbsRswkyRqBbVg',
+    error: function(err) {console.error(err)},
+    method: 'GET',
+    success: function(data){
+      return (data.results[0].geometry.viewport.northeast);
+    }
+  })
+}
