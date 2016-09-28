6787a0e212a303e6e098a351480a1dd1020990a3
Samuel Persian <samuel.persian@gmail.com>
2016-09-28 10:07:59 -0600

    load over https

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -108,7 +108,7 @@ function showAllLocationsWithin(nMiles) {
 // Adds a marker to the map and push to the array.
 function addMarker(l) {
   var pinColor = "4286f4";
-  var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+  var pinImage = new google.maps.MarkerImage("https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
       new google.maps.Size(21, 34),
       new google.maps.Point(0,0),
       new google.maps.Point(10, 34));
