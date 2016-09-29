f308761897a4369ff04d92febe851a7b53b64e64
Maia Samuel <maiasamuel98@gmail.com>
2016-09-29 10:25:00 -0600

    rendered layout instead of index but still not working

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ router.get('/', function (req, res, next) {
   return knex('user')
   .then(function(user) {
       console.log(user);
-      res.render('index', {user: user});
+      res.render('layout', {user: user});
     })
    .catch(function(error) {return next(error);
   });
