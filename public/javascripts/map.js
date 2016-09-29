4370d6d220ba7f2234fba9c80aed6e9b88d79ee6
Samuel Persian <samuel.persian@gmail.com>
2016-09-29 09:11:24 -0600

    ready for heroku

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -27,8 +27,8 @@ function initMap() {
       //   position: myLatLng,
       //   title: 'You are here!'
       // });
-      // $.get('https://hipfoodtrucks.herokuapp.com/today/locations')
-      $.get('http://localhost:3000/today/locations')
+      $.get('https://hipfoodtrucks.herokuapp.com/today/locations')
+      // $.get('http://localhost:3000/today/locations')
       .then((data) => {
         console.log("this is what our get to /today/locations returns ",data);
         let promises = [];
