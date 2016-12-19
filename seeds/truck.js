95d845fcfb9e660d0befc1f5ec6f92a7c2cc1266
Samuel Persian <samuel.persian@gmail.com>
2016-12-19 15:13:56 -0600

    created seed data for minneapolis, mn

--- seeds/truck.js
+++ seeds/truck.js
@@ -25,6 +25,27 @@ exports.seed = function(knex, Promise) {
           image_url: 'https://static.vecteezy.com/system/resources/previews/000/098/628/original/vector-seafood-truck.jpg',
           genre: 'Seafood',
           description: 'We bring the sea to street. Find all of your favorite seafood here!'
+        }),
+        knex('truck').insert({
+          owner_id:4,
+          truck_name:"Schwarma My Bones",
+          image_url: 'https://cbsnewyork.files.wordpress.com/2013/01/shawarma-spits.jpg',
+          genre: 'Mediterranean',
+          description: 'Steaming, seasoned, shaved, savory. If it\'s good enough for the Avengers, it must be worth a try!'
+        }),
+        knex('truck').insert({
+          owner_id:5,
+          truck_name:"Burger Food Truck",
+          image_url: 'https://upload.wikimedia.org/wikipedia/en/d/d1/CaliBurger_food_truck_Germany.jpg',
+          genre:'Hamburgers',
+          description: 'This truck sells hamburgers like they make them in California.'
+        }),
+        knex('truck').insert({
+          owner_id:6,
+          truck_name:"Sea On Wheels",
+          image_url: 'https://static.vecteezy.com/system/resources/previews/000/098/628/original/vector-seafood-truck.jpg',
+          genre: 'Seafood',
+          description: 'We bring the sea to street. Find all of your favorite seafood here!'
         })
       ]);
     });
