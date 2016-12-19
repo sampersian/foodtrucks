89b66c44f6c19ffb290539ac1931fe38fa9298d8
Samuel Persian <samuel.persian@gmail.com>
2016-12-19 15:58:39 -0600

    changed initial radius to 30 miles instead of 15

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -55,7 +55,7 @@ function initMap() {
           }
         })
         .then(() => {
-          showAllLocationsWithin(15);
+          showAllLocationsWithin(30);
         })
       } else if (maptype === "truck") {
         $.get('https://hipfoodtrucks.herokuapp.com/api/schedule/'+anId)
